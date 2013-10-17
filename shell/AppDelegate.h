6422946bf2705bb06c15c9851e3a35826608da9b
Nidhi Kulkarni <nkulkarni@okta.com>
2013-10-16 19:11:26 -0700

    Initial Commit

---
+++ shell/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  shell
+//
+//  Created by Nidhi Kulkarni on 10/16/13.
+//  Copyright (c) 2013 Nidhi Kulkarni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
